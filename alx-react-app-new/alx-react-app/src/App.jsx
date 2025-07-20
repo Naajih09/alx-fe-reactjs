5ca8e035cf7925e5302b11bbcd01950f98e4e4ad
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-07-20 03:57:01 +0100

    UseStates

--- alx-react-app-new/alx-react-app/src/App.jsx
+++ alx-react-app-new/alx-react-app/src/App.jsx
@@ -6,7 +6,8 @@ import WelcomeMessage from "./components/WelcomeMessage"
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import UserProfile from './components/UserProfile';
+import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <Counter />
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">
