1ca8163229a8c496e7816c95dcab26a7bf67790f
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-03 21:59:52 +0100

    Updates

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com";
 
+// Advanced search for users
 export const searchUsers = async (query) => {
-  const response = await axios.get(`${BASE_URL}/search/users`, {
-    params: { q: query },
-  });
+  // query should include "location" or "repos" if specified
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
   return response.data;
 };
