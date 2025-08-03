8022b936f25fcd8d56d9af7f2f32c98e5e01243a
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-03 21:46:08 +0100

    Step 4

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
+const BASE_URL = "https://api.github.com";
+
+export const searchUsers = async (query) => {
+  const response = await axios.get(`${BASE_URL}/search/users`, {
+    params: { q: query },
+  });
   return response.data;
 };
