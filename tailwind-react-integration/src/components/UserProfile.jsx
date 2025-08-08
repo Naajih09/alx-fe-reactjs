b27e22e1f8f2e58b0f3a295c75aef1b53ab5085a
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-08 21:08:39 +0100

    Interactive update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ import Naajih from "../components/Naajih.png";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center hover:shadow-xl">
       <img
         src={Naajih}
         alt="User"
-        className="sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto rounded-full object-cover"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto rounded-full object-cover hover:scale-110 transition-transform duration-300 ease-in-out"
       />
 
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
         Naajih Siraj
       </h1>
 
