677dba43228b0513d09e00fcd01baeaf28dbb9d3
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-08 16:00:29 +0100

    Updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
     <div className="user-profile" class="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
       <img src={Naajih} alt="User" class="w-36 h-36 mx-auto" />
       <h1 class="text-xl text-blue-800 my-4">Naajih Siraj</h1>
-      <p class="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p class="text-gray-600 text-base">Developer at MarkazCo. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
