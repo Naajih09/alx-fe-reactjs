b7c1b7e4e9c07e657d631f5a83d0a7dc39968cd1
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-08 20:52:36 +0100

    Responsivity

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,24 @@
+import Naajih from "../components/Naajih.png";
 
-import Naajih from "../components/Naajih.png"
 function UserProfile() {
   return (
-    <div className="user-profile" class="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
-      <img src={Naajih} alt="User" class="w-36 h-36 mx-auto rounded-full" />
-      <h1 class="text-xl text-blue-800 my-4">Naajih Siraj</h1>
-      <p class="text-gray-600 text-base">Developer at MarkazCo. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      {/* Profile Image */}
+      <img
+        src={Naajih}
+        alt="User"
+        className="w-24 h-24 md:w-36 md:h-36 mx-auto rounded-full object-cover"
+      />
+
+      {/* Name */}
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">
+        Naajih Siraj
+      </h1>
+
+      {/* Description */}
+      <p className="text-gray-600 text-sm md:text-base">
+        Developer at MarkazCo. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
