5f54b95b169543dfb2d47436a1cbbce3ed8466c5
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-08-08 15:57:32 +0100

    Tailwind implementation

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+
+import Naajih from "../components/Naajih.png"
+function UserProfile() {
+  return (
+    <div className="user-profile" class="bg-grey-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
+      <img src={Naajih} alt="User" class="w-36 h-36 mx-auto" />
+      <h1 class="text-xl text-blue-800 my-4">Naajih Siraj</h1>
+      <p class="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+  );
+}
+
+export default UserProfile;
